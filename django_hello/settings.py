43584a4af5eed66dd67bbfa3204376e6db45963a
Stephen Ong <stephen2005@ong.id.au>
2021-07-03 13:48:17 +1000

    post build

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '12345'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = ['stephenongpython36.onglab.com', '0.0.0.0']
+
+# [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 
 # Application definition
